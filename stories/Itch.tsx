b89d07f5e3fd6fc0bb80431449b437f1fbe7143b
Tamás Ridinger <ridinger.tamas@gmail.com>
2022-05-17 12:33:10 +0200

    Storybook config, Storybook example, some necessary changes in the Timeline.scss and the tsconfig.json

---
+++ stories/Itch.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import Timeline from "../src";
+import "../src/Timeline.scss";
+import moment from "moment";
+
+const groups = [
+    { id: 1, title: "group 1" },
+    { id: 2, title: "group 2" },
+];
+
+const items = [
+    {
+        id: 1,
+        group: 1,
+        title: "item 1",
+        start_time: moment(),
+        end_time: moment().add(1, "hour"),
+    },
+    {
+        id: 2,
+        group: 2,
+        title: "item 2",
+        start_time: moment().add(-0.5, "hour"),
+        end_time: moment().add(0.5, "hour"),
+    },
+    {
+        id: 3,
+        group: 1,
+        title: "item 3",
+        start_time: moment().add(2, "hour"),
+        end_time: moment().add(3, "hour"),
+    },
+];
+
+/**
+ * Primary UI component for user interaction
+ */
+export const Itch = ({}: {
+    /* Empty */
+}): JSX.Element => {
+    return (
+        <div>
+            <Timeline
+                groups={groups}
+                items={items}
+                defaultTimeStart={moment().add(-12, "hour")}
+                defaultTimeEnd={moment().add(12, "hour")}
+            />
+        </div>
+    );
+};
